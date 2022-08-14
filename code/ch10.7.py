17cc58e66d8a4b006e3c2585f633ab5d8f88dba9
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2022-08-14 06:51:00 +0200

    Solution to exercise 10.7

--- code/ch10.7.py
+++ code/ch10.7.py
@@ -5,11 +5,11 @@ from scipy.stats import norm
 
 # Exercise 10.7.a
 
-x0 = [.225, .262, .217, .240, .230, .229, .235, .217]
-x1 = [.209, .205, .196, .210, .202, .207, .224, .223, .220, .201]
+x = [.225, .262, .217, .240, .230, .229, .235, .217]
+y = [.209, .205, .196, .210, .202, .207, .224, .223, .220, .201]
 
-delta_hat = np.mean(x0) - np.mean(x1)
-var_hat = np.var(x0) / len(x0) + np.var(x1) / len(x1)
+delta_hat = np.mean(x) - np.mean(y)
+var_hat = np.var(x) / len(x) + np.var(y) / len(y)
 se_hat = np.sqrt(var_hat)
 
 W = delta_hat / se_hat
@@ -23,14 +23,14 @@ print(f'Confidence interval: ({delta_hat - 2*se_hat:.3f}, {delta_hat + 2*se_hat:
 num_simulations = 10**5
 rng = np.random.RandomState(37)
 
-data = np.concatenate([x0, x1])
+data = np.concatenate([x, y])
 t_obs = abs(delta_hat)
 t_perms = np.empty([num_simulations])
 for i in range(num_simulations):
     rng.shuffle(data)
-    x = data[0:len(x0)]
-    y = data[len(x0):]
-    t_perms[i] = abs(np.mean(x) - np.mean(y))
+    x_ = data[0:len(x)]
+    y_ = data[len(x):]
+    t_perms[i] = abs(np.mean(x_) - np.mean(y_))
 
 print(f'P-value: {np.sum(t_perms > t_obs) / num_simulations:.5f}')
 
