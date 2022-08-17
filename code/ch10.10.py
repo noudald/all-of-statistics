3e086c4ae180f345bc5c74ac3695ae22614c17d4
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2022-08-17 06:17:37 +0200

    Two other solutions for exercise 10.10

--- code/ch10.10.py
+++ code/ch10.10.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.stats import norm
+from scipy.stats import norm, binom_test, chisquare, chi2
 
 
 data = np.array([
@@ -28,3 +28,29 @@ threshold = np.sort(p_values)[r]
 
 for w, p in zip([-2, -1, 1, 2], p_values):
     print(f'Week {w:>2} p-value {p:.4f} reject {p <= threshold}')
+
+
+# Test 2, Binomial test
+print('\nTest 2, binomial test per week with BH corrected p-values')
+theta_hat = data[:, 0].sum() / data.sum()
+p_values = np.array([
+    binom_test(data[i, 0], data[i, :].sum(), theta_hat)
+       for i in range(len(data))
+])
+
+# Benjamini-Hochberg correction
+alpha = 0.05
+step = 1 / data.shape[0]
+r = np.argwhere(np.sort(p_values) < alpha * np.arange(step, 1.0 + step, step))[-1][0]
+threshold = np.sort(p_values)[r]
+
+for w, p in zip([-2, -1, 1, 2], p_values):
+    print(f'Week {w:>2} p-value {p:.4f} reject {p <= threshold}')
+
+
+# Test 3, X^2-test
+print('\nTest 3, X^2-test')
+theta_0 = data[:, 0].sum() / data.sum()
+chi2_ = ((data[:, 0] - theta_0 * data.sum(axis=1))**2 / (theta_0 * data.sum(axis=1))).sum()
+df = len(data) - 1
+print(f'p-value: {1 - chi2.cdf(chi2_, df):.5f}')
